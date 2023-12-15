c7c72fb875f1d7d0e5cd212346fb4fe1785a4471
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-12-15 15:59:45 +0530

    Add files via upload

---
+++ 9.Let , const & var Keywords/index.js
@@ -0,0 +1,92 @@
+// Let Keyword
+
+
+// Syntax for declaring variables
+/*
+let age = 23;
+age = age + 1;
+
+let cgpa;
+cgpa  = 8.9;
+
+let num1 = 1;
+let num2 = 2;
+let sum = num1 + num2;
+
+*/
+/*
+let a = 5 
+console.log(a); // 5
+
+let b;
+console.log(b);// udefined
+
+*/
+
+/*
+let age = 23;
+console.log(age);// 23
+
+age = age + 1;
+console.log(age); // 24
+
+age = age - 1;
+console.log(age); // 23
+
+let cgpa = 8.9;
+cgpa = 8.9 * 10;
+console.log(cgpa); // 89
+
+// square
+let side = 4;
+area = side ** 2
+console.log(area); // 16
+
+let num1 = 1;
+let num2 = 2;
+let finalSum = num1 + num2;
+console.log(finalSum); // 3
+
+*/
+
+// Const Keyword
+/*
+values of constant can't be changed with re-assignment & they can't be re declared.
+
+ */
+/*
+const  year = 2025;
+year =  2026 // error
+year = year + 1;
+
+const pi = 3.14;
+const g = 9.8;
+*
+/
+/*
+const pi = 3.14;
+
+let rad = 4;
+
+let area = pi * rad ** 2
+console.log(area); // 50.24
+
+*/
+
+// Var Keyword
+// Old Syntax of writing variables
+/*
+var age = 23;
+var cgpa =  8.9
+
+var num1 = 1;
+var num2 = 2;
+var sum = num1 + num2;
+*/
+
+var x = 5;
+var y = 10;
+
+var sum = x + y;
+console.log(sum); // 15
+
