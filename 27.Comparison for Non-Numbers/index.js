0b0c2b0b012ad32da75b223c0dd71b244af2140a
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-12-18 20:29:09 +0530

    Add files via upload

---
+++ 27.Comparison for Non-Numbers/index.js
@@ -0,0 +1,17 @@
+// Comparison for non- numbers
+
+console.log('a' > 'A'); // true
+
+console.log('a' > 'b'); // false
+
+console.log('b' < 'c'); //true
+
+console.log('B' < 'C'); // true
+
+console.log('*' < '&'); // false
+
+// 'a' --> 61 ,'b' --> 62 , ......
+
+// 'A' --> 41 , 'B' --> 42 , .....
+
+// It is based on unicode
