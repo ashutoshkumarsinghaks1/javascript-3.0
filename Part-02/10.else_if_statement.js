bafbdbcfe766fa5534523ed479993f8d19bcefe4
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-12-22 21:19:03 +0530

    Add files via upload

---
+++ Part-02/10.else_if_statement.js
@@ -0,0 +1,117 @@
+// Else if statement
+/*
+if(condition1){
+    //do something
+}
+else if(condition2){
+    //do something else
+}
+else if(condition3){
+    //do something else
+}
+*/
+
+/*
+let age = 14;
+if(age >= 18){
+    console.log("you can vote");
+}
+else if (age < 18){
+    console.log("you cannot vote");
+}
+
+// output : you cannot vote
+*/
+
+/*
+let age = 23;
+if (age >= 18){
+    console.log("You can vote");
+}
+else if (age >=18){
+    console.log("You cannot vote");
+}
+
+/ output: You can vote
+ agar phle if rahega aur baad me else if rahega condition true hone ke baad bhi else if execute nhi hoga
+/ agar if ke baad aur if condition rahega aur true rahega to execute hoga
+
+*/
+
+/*
+let age = 23;
+if (age >= 18){
+    console.log("You can vote");
+}
+if (age >=18){
+    console.log("You can vote");
+}
+
+//Output:
+// You can vote
+//You can vote
+*/
+
+
+/*
+let age = 14;
+if (age >= 18){
+    console.log("you can vote");
+}else if (age >= 18){
+    console.log("You cannot vote");
+}else if(age < 18){
+    console.log("You cannot vote2");
+}
+
+// output: You cannot vote2
+// else if gives ouput of condition for which if is false.
+
+*/
+
+/*
+let marks = 75;
+
+if(marks >= 80){
+    console.log("A+");
+}
+else if (marks >= 60){
+    console.log("A");
+}
+else if (marks >= 33){
+    console.log("B");
+}
+else if (marks < 33){
+    console.log("F");
+}
+
+// Output : A
+
+*/
+
+/*
+let month = "January";
+
+if(month === "January"){
+    console.log("winter is here");
+}
+else if (month === "April"){
+    console.log("summer is here");
+}
+
+// output: winter is here
+*/
+
+/*
+let month = "April";
+
+if(month === "January"){
+    console.log("winter is here");
+}
+else if (month === "April"){
+    console.log("summer is here");
+}
+
+// output: summer is here
+
+*/
+
---
+++ Part-02/11.else_statement.js
@@ -0,0 +1,113 @@
+// else statement
+
+/*
+if (condition1){
+    // do something
+}
+
+else {
+    // do something else
+}
+
+*/
+
+/*
+let age = 18;
+
+if (age >= 18){
+    console.log("you can vote");
+}
+else{
+    console.log("you cannot vote");
+}
+
+// output : you can vote
+*/
+
+/*
+let age = 17;
+
+if (age >= 18){
+    console.log("you can vote");
+}
+else{
+    console.log("you cannot vote");
+}
+
+// output : you cannot vote
+
+*/
+
+/*
+
+let color = "red";
+
+if(color === "red"){
+    console.log("stop");
+}
+else if ( color === "yellow"){
+    console.log("slow down");
+}
+else if (color === "green"){
+    console.log("go!");
+}
+else{
+    console.log("traffic light is broken");
+}
+
+//output:  stop
+
+*/
+
+/*
+let color = "yellow";
+
+if(color === "red"){
+    console.log("stop");
+}
+else if ( color === "yellow"){
+    console.log("slow down");
+}
+else if (color === "green"){
+    console.log("go!");
+}
+else{
+    console.log("traffic light is broken");
+}
+
+// output : slow down
+*/
+
+/*
+let color = "green";
+
+if(color === "red"){
+    console.log("stop");
+}
+else if ( color === "yellow"){
+    console.log("slow down");
+}
+else if (color === "green"){
+    console.log("go!");
+}
+else{
+    console.log("traffic light is broken");
+}
+
+// output : go!
+
+*/
+
+let color = "white";
+
+if (color === "red") {
+  console.log("stop");
+} else if (color === "yellow") {
+  console.log("slow down");
+} else if (color === "green") {
+  console.log("go!");
+} else {
+  console.log("traffic light is broken");
+}
+
+// output : traffic light is broken
