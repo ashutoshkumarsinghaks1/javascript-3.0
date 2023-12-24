8739c7c327be537e054c538e0a8c283e2a640b3f
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-12-24 20:59:59 +0530

    Update 12.Practice-qs.js

---
+++ Part-02/12.Practice-qs.js
@@ -0,0 +1,13 @@
+let color = "white";
+
+if (color === "red") {
+  console.log("stop");
+} else if (color === "yellow") {
+  console.log("slow down");
+} else if (color === "green") {
+  console.log("go!");
+} else {
+  console.log("traffic light is broken");
+}
+
+// output : traffic light is broken
