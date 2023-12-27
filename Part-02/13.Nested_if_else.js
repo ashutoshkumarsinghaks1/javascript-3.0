5a8fcc3992c5d67bf5bc2a360d9095afec32159b
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-12-27 22:59:29 +0530

    Add files via upload

---
+++ Part-02/13.Nested_if_else.js
@@ -0,0 +1,16 @@
+//Nested if else
+/* Nesting is writing if-else inside if-else statements. It can have many
+levels
+*/
+
+/*
+if marks = 33
+   if marks >=30
+       print "0"
+
+   else
+       print "A"
+
+else
+   print "better luck next time!"
+*/
