530fc5ae918cba138b6eeb6a3e69b6ef156d9fea
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-12-28 18:16:25 +0530

    Update 12.Practice-qs.js

--- Part-02/12.Practice-qs.js
+++ Part-02/12.Practice-qs.js
@@ -11,6 +11,27 @@ if (color === "red") {
 }
 
 // output : traffic light is broken
+// Pratice Qs
+/*Create a system to calculate popcorn prices based on the size customer asked for:
+
+if size is "XL", price is Rs 250
+if size is "L",  price is Rs 200
+if size is "M",  price is Rs 100
+if size is "S",  price is Rs 50
+*/
+let size = "XL";
+
+if (size === "XL") {
+  console.log("price is Rs 250");
+} else if (size === "L") {
+  console.log("price is Rs 200");
+} else if (size === "M") {
+  console.log("price is Rs 100");
+} else {
+  console.log("price is Rs 50");
+}
+
+// output: price is Rs 250
 
 let size = "S";
 
