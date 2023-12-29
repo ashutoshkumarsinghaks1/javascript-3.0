7fb84bd932248d75d0ba27fa12b4623a255423d9
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-12-29 22:03:26 +0530

    Update 13.Nested_if_else.js

--- Part-02/13.Nested_if_else.js
+++ Part-02/13.Nested_if_else.js
@@ -14,3 +14,15 @@ if marks = 33
 else
    print "better luck next time!"
 */
+let marks = 45;
+
+if(marks >= 33){
+    console.log("Pass");
+    if(marks >= 80){
+        console.log("Grade : 0");
+    }else{
+        console.log("Grade: A");
+    }
+}else{
+    console.log("Better luck next time!");
+}
