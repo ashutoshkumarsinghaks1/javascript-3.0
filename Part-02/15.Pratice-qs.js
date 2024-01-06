115c3d5cb9930545e17eabb9702f5e9527adb474
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2024-01-06 23:04:53 +0530

    Add files via upload

---
+++ Part-02/15.Pratice-qs.js
@@ -0,0 +1,55 @@
+// Practice Qs
+// Q1. A "good string" is a string  that starts with the letter 'a' & has length > 3.  Write a Program to find if the string is good or not.
+
+/*
+let String1 = "apple";
+
+if (String1[0] === "a" && String1.length > 3) {
+  console.log("good string ");
+} else {
+  console.log("not a good string");
+}
+
+
+// output: good string
+*/
+
+/*
+let String1 = "ambition";
+
+if (String1[0] === "a" && String1.length > 3) {
+  console.log("good string ");
+} else {
+  console.log("not a good string");
+}
+
+// output: good string
+*/
+
+/*
+let String1 = "am";
+
+if (String1[0] === "a" && String1.length > 3) {
+  console.log("good string ");
+} else {
+  console.log("not a good string");
+}
+
+//output: not a good string
+
+*/
+
+/*
+let String1 = "1ameen";
+
+if (String1[0] === "a" && String1.length > 3) {
+  console.log("good string ");
+} else {
+  console.log("not a good string");
+}
+
+
+// output: not a good string
+
+*/
+
