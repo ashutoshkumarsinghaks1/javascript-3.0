4938531db32fbfd8e81245276eac972958a3ba6a
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2024-01-09 23:09:01 +0530

    Update 01.console_log.js

--- Part-02/01.console_log.js
+++ Part-02/01.console_log.js
@@ -12,3 +12,4 @@ console.log("Your", "College", 123); // Your College 123
 
 console.log("Hello", "World", 1 + 8); // Hello World 9
 console.log("Ashutosh");// Ashutosh
+console.log("Singh");// Singh
